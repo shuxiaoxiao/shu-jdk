092ef8c7a1e58dc0466e030907b4f72c12ea069a
shuheng <shuheng@rangercn.com>
2019-03-28 17:26:22 +0800

    优化 冒泡排序

--- src/main/java/com/shuframework/arithmetic/BubbleSort.java
+++ src/main/java/com/shuframework/arithmetic/BubbleSort.java
@@ -3,32 +3,83 @@ package com.shuframework.arithmetic;
 import java.util.Arrays;
 
 /**
- * 冒泡排序
+ * 冒泡排序 每趟将最大值放到最后
+ * <p>
  * Created by shu on 2018/9/13.
  */
 public class BubbleSort {
 
-    public static void sort(int[] intArr){
+    //这个更高效 可以减少局部有序的比较次数
+    //第一次排序后部分已经有序, 那么开始比较的位置就往前了
+    public static void sort(int[] intArr) {
         //max为 数组长度-1
-        for (int i = 0, max = intArr.length - 1; i < max; i++) {
-//            System.out.println("第"+(i+1)+"趟");
+        int max = intArr.length - 1;
+        //记录上次发生交换位置的index
+        int secondCount = max;
+        for (int i = 0; i < max; i++) {
+            System.out.println("第" + (i + 1) + "趟");
+            //假设每一趟开始都是有序
+            boolean flag = true;
+            int lastChangeIndex = 0;
+            for (int j = 0; j < secondCount; j++) {
+                if (intArr[j] > intArr[j + 1]) {
+                    //2数互换
+                    int temp = intArr[j];
+                    intArr[j] = intArr[j + 1];
+                    intArr[j + 1] = temp;
+                    flag = false;
+                    lastChangeIndex = j;
+                }
+                System.out.println("比较次数："+(j+1)+", 结果:"+ Arrays.toString(intArr));
+            }
+            if (flag) {
+                break;
+            }
+            //循环完后 得到最后的发生交换时下标
+            secondCount = lastChangeIndex;
+        }
+    }
+
+    public static void sortHasFlag(int[] intArr) {
+        //max为 数组长度-1
+        int max = intArr.length - 1;
+        for (int i = 0; i < max; i++) {
+            System.out.println("第" + (i + 1) + "趟");
             //假设每一趟开始都是有序
             boolean flag = true;
             for (int j = 0; j < max - i; j++) {
-                if (intArr[j] > intArr[j+1]){
+                if (intArr[j] > intArr[j + 1]) {
                     //2数互换
                     int temp = intArr[j];
-                    intArr[j] = intArr[j+1];
-                    intArr[j+1] = temp;
+                    intArr[j] = intArr[j + 1];
+                    intArr[j + 1] = temp;
                     flag = false;
                 }
-//                System.out.println("结果:"+ Arrays.toString(intArr));
+                System.out.println("比较次数：" + (j + 1) + ", 结果:" + Arrays.toString(intArr));
             }
-            if (flag){
+            if (flag) {
                 break;
             }
         }
-
     }
+
+    // 缺点是已经有序时，后面的次数还是会继续
+    public static void sortClassic(int[] intArr) {
+        //max为 数组长度-1
+        int max = intArr.length - 1;
+        for (int i = 0; i < max; i++) {
+            System.out.println("第" + (i + 1) + "趟");
+            for (int j = 0; j < max - i; j++) {
+                if (intArr[j] > intArr[j + 1]) {
+                    //2数互换
+                    int temp = intArr[j];
+                    intArr[j] = intArr[j + 1];
+                    intArr[j + 1] = temp;
+                }
+                System.out.println("比较次数：" + (j + 1) + ", 结果:" + Arrays.toString(intArr));
+            }
+        }
+    }
+
 
 }
--- src/test/java/com/shuframework/arithmetic/SortTest.java
+++ src/test/java/com/shuframework/arithmetic/SortTest.java
@@ -10,10 +10,35 @@ import java.util.Arrays;
 public class SortTest {
 
     @Test
+    public void test2(){
+        int[] intArr1 = {3,1,2,6,4,5};
+        BubbleSort.sort(intArr1);
+//        System.out.println("结果:"+ Arrays.toString(intArr));
+        System.out.println("=============");
+        int[] intArr2 = {3,1,2,6,4,5};
+        BubbleSort.sortHasFlag(intArr2);
+    }
+
+    @Test
     public void test1(){
-//        int[] intArr = {3,2,6,4,5,9,8};
-        int[] intArr = {3,2,9,6,4,5,8};
+        int[] intArr1 = {3,2,1,6,4,5,7,8};
+        BubbleSort.sort(intArr1);
+//        System.out.println("结果:"+ Arrays.toString(intArr));
+        System.out.println("=============");
+
+        int[] intArr2 = {3,2,1,6,4,5,7,8};
+        BubbleSort.sortHasFlag(intArr2);
+        System.out.println("=============");
+
+        int[] intArr3 = {3,2,1,6,4,5,7,8};
+        BubbleSort.sortClassic(intArr3);
+    }
+
+    @Test
+    public void test20(){
+        int[] intArr = {3,2,5,4,6,7,8,9};
         BubbleSort.sort(intArr);
         System.out.println("结果:"+ Arrays.toString(intArr));
     }
+
 }
